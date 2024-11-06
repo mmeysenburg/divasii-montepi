9809d92b9b20c1c64083384534cd56ac9af39975
Mark Meysenburg <mark.meysenburg@doane.edu>
2024-11-06 10:44:06 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+#DIVAS Seminar II
+
+##HTC with OSG introduction
+
+###Monte Carlo estimation of π
+
+After cloning this repository, follow the directions in the 
+`montepi.py` file to complete the Monte Carlo program. Then,
+use the `condor_submit montepi.submit` command to launch ten
+instances of the program. 
+
+Once all ten instances have finished, download the `.out` files
+and compute your estimate of π. 
---
+++ montepi.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+'''
+Edit the code below to throw 10,000,000 random darts in 
+[0, 1) at our unit-circle quadrant, and print the number
+of those darts that landed in the circle.
+'''
+import random
+
+n = 10_000_000
+num_in = 0
+
+# Add code here to throw n darts. Use the num_in
+# variable to count how many of them land inside
+# the unit circle, using the formula we developed.
+
+print(num_in)
